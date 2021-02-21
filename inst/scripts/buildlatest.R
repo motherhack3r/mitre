0dff9f0b0fefc5794f1c656c9b42af3c8d50a546
Humbert <humbert.costas@gmail.com>
2021-02-22 00:05:23 +0100

    Added script to build and save datasets

---
+++ inst/scripts/buildlatest.R
@@ -0,0 +1,19 @@
+library(mitre)
+
+today <- as.character(Sys.Date())
+file.current <- paste0(today, "_mitre_v", packageVersion("mitre"), ".rds")
+file.latest <- "mitre_latest.rds"
+
+print("START")
+
+mitredata <- mitre::parseRawData(verbose = TRUE)
+
+print(paste0("NODES: ", nrow(mitredata$mitrenet$nodes)))
+print(paste0("EDGES: ", nrow(mitredata$mitrenet$edges)))
+
+print(paste0("Saving in mitre-datasets/beta/", file.current))
+saveRDS(object = mitredata, file = paste0("../../../datasets/mitre-datasets/beta/", file.current))
+print(paste0("Saving in mitre-datasets/latest/", file.latest))
+saveRDS(object = mitredata, file = paste0("../../../datasets/mitre-datasets/latest/", file.latest))
+
+print("END")
