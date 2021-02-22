ddc4d456d3cbb06fdb7f3af3f93e0f937c86e53d
Humbert <humbert.costas@gmail.com>
2021-02-22 23:28:46 +0100

    Minor change

--- inst/scripts/buildlatest.R
+++ inst/scripts/buildlatest.R
@@ -4,10 +4,11 @@ today <- as.character(Sys.Date())
 file.current <- paste0(today, "_mitre_v", packageVersion("mitre"), ".rds")
 file.latest <- "mitre_latest.rds"
 
-print("START")
+print(paste0("START at ", as.character(Sys.time())))
 
 mitredata <- mitre::parseRawData(verbose = TRUE)
 
+print(paste0("Parsed at ", as.character(Sys.time())))
 print(paste0("NODES: ", nrow(mitredata$mitrenet$nodes)))
 print(paste0("EDGES: ", nrow(mitredata$mitrenet$edges)))
 
@@ -16,4 +17,4 @@ saveRDS(object = mitredata, file = paste0("../../../datasets/mitre-datasets/beta
 print(paste0("Saving in mitre-datasets/latest/", file.latest))
 saveRDS(object = mitredata, file = paste0("../../../datasets/mitre-datasets/latest/", file.latest))
 
-print("END")
+print(paste0("END at ", as.character(Sys.time())))
