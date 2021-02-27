e91d0397b903c202ddec2b45857223fb2f84cb39
Humbert <humbert.costas@gmail.com>
2021-02-27 16:34:11 +0100

    Minor improvement

--- inst/scripts/buildlatest.R
+++ inst/scripts/buildlatest.R
@@ -3,6 +3,7 @@ library(mitre)
 today <- as.character(Sys.Date())
 file.current <- paste0(today, "_mitre_v", packageVersion("mitre"), ".rds")
 file.latest <- "mitre_latest.rds"
+path.datasets <- "C:/DEVEL/datasets/mitre-datasets/"
 
 print(paste0("START at ", as.character(Sys.time())))
 
@@ -12,9 +13,9 @@ print(paste0("Parsed at ", as.character(Sys.time())))
 print(paste0("NODES: ", nrow(mitredata$mitrenet$nodes)))
 print(paste0("EDGES: ", nrow(mitredata$mitrenet$edges)))
 
-print(paste0("Saving in mitre-datasets/beta/", file.current))
-saveRDS(object = mitredata, file = paste0("../../../datasets/mitre-datasets/beta/", file.current))
-print(paste0("Saving in mitre-datasets/latest/", file.latest))
-saveRDS(object = mitredata, file = paste0("../../../datasets/mitre-datasets/latest/", file.latest))
+print(paste0("Saving in ", path.datasets, "beta/", file.current))
+saveRDS(object = mitredata, file = paste0(path.datasets, "beta/", file.current))
+print(paste0("Saving in ", path.datasets, "latest/", file.latest))
+saveRDS(object = mitredata, file = paste0(path.datasets, "latest/", file.latest))
 
 print(paste0("END at ", as.character(Sys.time())))
