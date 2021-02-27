03062544ed4c4af9ed0537fdb9a8a98916a4baab
Humbert <humbert.costas@gmail.com>
2021-02-27 19:37:11 +0100

    Added more schema references

--- inst/scripts/buildsample.R
+++ inst/scripts/buildsample.R
@@ -1,3 +1,10 @@
 # Schemas:
 #  - CVE: https://csrc.nist.gov/schema/nvd/feed/1.1/nvd_cve_feed_json_1.1.schema
 #  - CWE: https://cwe.mitre.org/data/xsd/cwe_schema_latest.xsd
+#  - CPE: https://csrc.nist.gov/schema/cpe/2.3/cpe-dictionary_2.3.xsd
+#         https://csrc.nist.gov/schema/cpe/2.3/cpe-dictionary-extension_2.3.xsd
+#         https://csrc.nist.gov/schema/cpe/2.2/cpe-dictionary_2.2.xsd
+#         https://csrc.nist.gov/schema/cpe/2.1/cpe-dictionary-metadata_0.2.xsd
+#  - CAPEC: https://capec.mitre.org/data/xsd/ap_schema_latest.xsd
+#           https://github.com/mitre/cti/blob/master/USAGE-CAPEC.md
+#  - ATTCK: https://github.com/mitre/cti/blob/master/USAGE.md
