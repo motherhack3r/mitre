cd723b545c4e3eb9e5a6d79a18e76ca13075ba81
Humbert <humbert.costas@gmail.com>
2021-03-13 20:47:06 +0100

    Removed testing scripts

--- inst/scripts/buildigraph.R
+++
@@ -1,7 +0,0 @@
-mitredata <- readRDS("C:/DEVEL/datasets/mitre-datasets/latest/mitre_latest.rds")
-
-mitrenet <- mitredata$mitrenet
-
-nodes <- mitrenet$nodes
-
-nodes <- nodes[which(!nodes$shadow),]
