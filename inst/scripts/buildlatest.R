e402af0c6a44b5d8b4080821727c76f57110c8dd
Humbert <humbert.costas@gmail.com>
2021-03-15 21:44:43 +0100

    Rscript buildlatest.R now as Rscript which accepts parameter to define output path.

--- inst/scripts/buildlatest.R
+++ inst/scripts/buildlatest.R
@@ -1,24 +1,50 @@
-library(mitre)
+#!/usr/bin/env Rscript
+list.of.packages <- c("optparse", "mitre")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org", verbose = FALSE)
+
+suppressPackageStartupMessages(library("optparse"))
+
+option_list <- list(
+  optparse::make_option(c("-v", "--verbose"), action="store_true", default=FALSE,
+                        help="Print extra output [default FALSE]"),
+  optparse::make_option(c("-o", "--output"), default="./mitre-datasets",
+                        help = "Path where files will be saved [default \"%default\"]")
+)
 
 today <- as.character(Sys.Date())
-path.latest <- file.path("c:","DEVEL","datasets","mitre-datasets")
-file.current <- paste0(today, "_mitre_v", packageVersion("mitre"), ".rds")
-file.latest <- "mitre_latest.rds"
-path.datasets <- "C:/DEVEL/datasets/mitre-datasets/"
 
-print(paste0("START at ", as.character(Sys.time())))
+opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
+print(opt$verbose)
 
-mitredata <- mitre::parseRawData(verbose = TRUE)
+if (dir.exists(opt$output)) {
+  if (opt$verbose) print(paste0("START at ", as.character(Sys.time())))
+  path.latest <- file.path(opt$output, "latest")
+  path.beta <- file.path(opt$output, "beta")
+  file.latest <- "mitre_latest.rds"
+  file.beta <- paste0(today, "_mitre_v", packageVersion("mitre"), ".rds")
 
-print(paste0("Parsed at ", as.character(Sys.time())))
-print(paste0("NODES: ", nrow(mitredata$mitrenet$nodes)))
-print(paste0("EDGES: ", nrow(mitredata$mitrenet$edges)))
+  if (!dir.exists(path.latest)) dir.create(path.latest)
+  if (!dir.exists(path.beta)) dir.create(path.beta)
 
-file.save <- file.path(path.latest, "beta", file.current)
-print(paste0("Saving ", file.save))
-saveRDS(object = mitredata, file = file.save)
-file.save <- file.path(path.latest, "latest", file.latest)
-print(paste0("Saving ", file.save))
-saveRDS(object = mitredata, file = file.save)
+  library(mitre)
 
-print(paste0("END at ", as.character(Sys.time())))
+  # mitredata <- mitre::parseRawData(verbose = opt$verbose)
+  # if (opt$verbose) print(paste0("Parsed at ", as.character(Sys.time())))
+  # if (opt$verbose) print(paste0("NODES: ", nrow(mitredata$mitrenet$nodes)))
+  # if (opt$verbose) print(paste0("EDGES: ", nrow(mitredata$mitrenet$edges)))
+  #
+  # file.save <- file.path(path.beta, file.beta)
+  # if (opt$verbose) print(paste0("Saving ", file.save))
+  # saveRDS(object = mitredata, file = file.save)
+  #
+  # file.save <- file.path(path.latest, file.latest)
+  # if (opt$verbose) print(paste0("Saving ", file.save))
+  # saveRDS(object = mitredata, file = file.save)
+  #
+  # if (opt$verbose) print(paste0("END at ", as.character(Sys.time())))
+
+} else {
+  warning(paste0("Path: ", opt$output, " does NOT exist."))
+}
+
