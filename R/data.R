757a270be115e6b946ef47878b80bad0eacd5a67
Humbert <humbert.costas@gmail.com>
2021-05-16 13:42:45 +0200

    Update data documentation

--- R/data.R
+++ R/data.R
@@ -1,5 +1,117 @@
 NULL
 
+#' ATT&CK Groups Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"attck.groups"
+
+#' ATT&CK Mitigation Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"attck.mitigations"
+
+#' ATT&CK relations Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"attck.relations"
+
+#' ATT&CK software Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"attck.software"
+
+#' ATT&CK tactics Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"attck.tactics"
+
+#' ATT&CK techniques Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"attck.techniques"
+
+#' CAPEC categories Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"capec.categories"
+
+#' CAPEC views Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"capec.views"
+
+#' CAPEC patterns Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"capec.patterns"
+
+#' CAPEC relations Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"capec.relations"
+
+#' CAR analytics Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"car.analytics"
+
+#' CAR coverage Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"car.coverage"
+
+#' CAR implementations Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"car.implementations"
+
+#' CAR data model Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"car.model"
+
+#' CAR relations Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"car.relations"
+
+#' CAR sensors Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"car.sensors"
+
 #' Common Platform Enumeration.
 #'
 #' Full data set provided by NIST.
@@ -16,3 +128,68 @@ NULL
 #'   \code{description}, \code{vulnerable.configuration} which is related to CPE,
 #'   \code{references}, \code{cvss3}, \code{cvss2} and all separated values.
 "cve.nist"
+
+#' CWE categories Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"cwe.categories"
+
+#' CWE views Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"cwe.views"
+
+#' CWE Weaknesses Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"cwe.weaknesses"
+
+#' SHIELD tactics Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"shield.tactics"
+
+#' SHIELD techniques Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"shield.techniques"
+
+#' SHIELD opportunities Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"shield.opportunities"
+
+#' SHIELD procedures Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"shield.procedures"
+
+#' SHIELD relations Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"shield.relations"
+
+#' SHIELD use cases Objects.
+#'
+#' Full data set provided by MITRE
+#'
+#' @format A data frame with 11 variables.
+"shield.use_cases"
+
+
