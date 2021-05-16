2a797357eb6a7c76257d2a3e83d0b836cabb76bd
Humbert <humbert.costas@gmail.com>
2021-05-16 11:04:37 +0200

    Added some data sets documentation

--- R/data.R
+++ R/data.R
@@ -7,3 +7,12 @@ NULL
 #' @format A data frame with 16 variables: \code{title}, \code{cpe.22},
 #'   \code{cpe.23}, and all separated values.
 "cpe.nist"
+
+#' Common Vulnerability Enumeration.
+#'
+#' Full data set provided by NIST.
+#'
+#' @format A data frame with 34 variables: \code{cve.id}, \code{problem.type} which is related to CWE,
+#'   \code{description}, \code{vulnerable.configuration} which is related to CPE,
+#'   \code{references}, \code{cvss3}, \code{cvss2} and all separated values.
+"cve.nist"
