04d038695527e17dd561f851ce3e68ab94a11d49
Humbert <humbert.costas@gmail.com>
2021-05-18 22:41:39 +0200

    Add sample data sets as internal data

--- R/data.R
+++ R/data.R
@@ -11,21 +11,21 @@ NULL
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 12 variables.
 "attck.mitigations"
 
 #' ATT&CK relations Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 13 variables.
 "attck.relations"
 
 #' ATT&CK software Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 12 variables.
 "attck.software"
 
 #' ATT&CK tactics Objects.
@@ -39,77 +39,77 @@ NULL
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 15 variables.
 "attck.techniques"
 
 #' CAPEC categories Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 4 variables.
 "capec.categories"
 
 #' CAPEC views Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 5 variables.
 "capec.views"
 
 #' CAPEC patterns Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 16 variables.
 "capec.patterns"
 
 #' CAPEC relations Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 4 variables.
 "capec.relations"
 
 #' CAR analytics Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 17 variables.
 "car.analytics"
 
 #' CAR coverage Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 4 variables.
 "car.coverage"
 
 #' CAR implementations Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 7 variables.
 "car.implementations"
 
 #' CAR data model Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 8 variables.
 "car.model"
 
 #' CAR relations Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 2 variables.
 "car.relations"
 
 #' CAR sensors Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 5 variables.
 "car.sensors"
 
 #' Common Platform Enumeration.
@@ -133,63 +133,63 @@ NULL
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 7 variables.
 "cwe.categories"
 
 #' CWE views Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 7 variables.
 "cwe.views"
 
 #' CWE Weaknesses Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 24 variables.
 "cwe.weaknesses"
 
 #' SHIELD tactics Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 4 variables.
 "shield.tactics"
 
 #' SHIELD techniques Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 4 variables.
 "shield.techniques"
 
 #' SHIELD opportunities Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 2 variables.
 "shield.opportunities"
 
 #' SHIELD procedures Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 2 variables.
 "shield.procedures"
 
 #' SHIELD relations Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 3 variables.
 "shield.relations"
 
 #' SHIELD use cases Objects.
 #'
 #' Full data set provided by MITRE
 #'
-#' @format A data frame with 11 variables.
+#' @format A data frame with 2 variables.
 "shield.use_cases"
 
 
