2cd74268c9f92ae85fb17baeec89df3391ce331b
Humbert <humbert.costas@gmail.com>
2021-05-19 02:33:50 +0200

    Build sample data set for internal usage

--- data-raw/build_data.R
+++ data-raw/build_data.R
@@ -9,16 +9,27 @@ source("data-raw/mitre-shield.R")
 source("data-raw/nist-cpe.R")
 source("data-raw/nist-cve.R")
 
-# Sample data... it's so huge
+# Build sample data sets
 set.seed(42)
-cpe.nist <- dplyr::sample_n(cpe.nist, 500)
-cve.nist <- dplyr::sample_n(cve.nist, 1000)
-attck.relations <- dplyr::sample_n(attck.relations, 4000)
 
-usethis::use_data(cve.nist, cpe.nist, capec.views, capec.categories, capec.patterns,
-                  capec.relations, car.analytics, car.model, car.sensors, car.implementations,
-                  car.coverage, car.relations, shield.tactics, shield.techniques, shield.procedures,
-                  shield.use_cases, shield.opportunities, shield.relations, attck.tactics,
-                  attck.techniques, attck.mitigations, attck.groups,
-                  attck.software, attck.relations,
+cpe.nist <- dplyr::sample_frac(cpe.nist, 0.0002)
+cve.nist <- dplyr::sample_frac(cve.nist, 0.004)
+cwe.weaknesses <- cwe.weaknesses[cwe.weaknesses$Status != "Deprecated", ]
+cwe.weaknesses <- dplyr::sample_frac(cwe.weaknesses, 0.5)
+cwe.categories <- cwe.weaknesses[cwe.categories$Status != "Deprecated", ]
+cwe.categories <- dplyr::sample_frac(cwe.categories, 0.5)
+capec.patterns <- dplyr::sample_frac(capec.patterns, 0.5)
+attck.relations <- dplyr::sample_frac(attck.relations, 0.5)
+attck.techniques <- dplyr::sample_frac(attck.techniques, 0.5)
+attck.software <- dplyr::sample_frac(attck.software, 0.5)
+car.analytics <- dplyr::sample_frac(car.analytics, 0.5)
+car.model <- dplyr::sample_frac(car.model, 0.5)
+
+usethis::use_data(cve.nist, cpe.nist, cwe.views, cwe.categories, cwe.weaknesses,
+                  capec.views, capec.categories, capec.patterns, capec.relations,
+                  car.analytics, car.model, car.sensors, car.implementations,
+                  car.coverage, car.relations, shield.tactics, shield.techniques,
+                  shield.procedures, shield.use_cases, shield.opportunities,
+                  shield.relations, attck.tactics, attck.techniques, attck.mitigations,
+                  attck.groups, attck.software, attck.relations,
                   compress = "xz", overwrite = TRUE, internal = TRUE)
