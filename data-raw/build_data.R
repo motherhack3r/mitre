7056acf9bd46a85566db21095868bb1cd2125ba6
Humbert <humbert.costas@gmail.com>
2021-05-19 03:56:02 +0200

    Update data sets

--- data-raw/build_data.R
+++ data-raw/build_data.R
@@ -10,20 +10,19 @@ source("data-raw/nist-cpe.R")
 source("data-raw/nist-cve.R")
 
 # Build sample data sets
-set.seed(42)
-
-cpe.nist <- dplyr::sample_frac(cpe.nist, 0.0002)
-cve.nist <- dplyr::sample_frac(cve.nist, 0.004)
-cwe.weaknesses <- cwe.weaknesses[cwe.weaknesses$Status != "Deprecated", ]
-cwe.weaknesses <- dplyr::sample_frac(cwe.weaknesses, 0.5)
-cwe.categories <- cwe.weaknesses[cwe.categories$Status != "Deprecated", ]
-cwe.categories <- dplyr::sample_frac(cwe.categories, 0.5)
-capec.patterns <- dplyr::sample_frac(capec.patterns, 0.5)
-attck.relations <- dplyr::sample_frac(attck.relations, 0.5)
-attck.techniques <- dplyr::sample_frac(attck.techniques, 0.5)
-attck.software <- dplyr::sample_frac(attck.software, 0.5)
-car.analytics <- dplyr::sample_frac(car.analytics, 0.5)
-car.model <- dplyr::sample_frac(car.model, 0.5)
+# set.seed(42)
+# cpe.nist <- dplyr::sample_frac(cpe.nist, 0.0002)
+# cve.nist <- dplyr::sample_frac(cve.nist, 0.005)
+# cwe.weaknesses <- cwe.weaknesses[cwe.weaknesses$Status != "Deprecated", ]
+# cwe.weaknesses <- dplyr::sample_frac(cwe.weaknesses, 0.6)
+# cwe.categories <- cwe.weaknesses[cwe.categories$Status != "Deprecated", ]
+# cwe.categories <- dplyr::sample_frac(cwe.categories, 0.6)
+# capec.patterns <- dplyr::sample_frac(capec.patterns, 0.6)
+# attck.relations <- dplyr::sample_frac(attck.relations, 0.6)
+# attck.techniques <- dplyr::sample_frac(attck.techniques, 0.6)
+# attck.software <- dplyr::sample_frac(attck.software, 0.6)
+# car.analytics <- dplyr::sample_frac(car.analytics, 0.6)
+# car.model <- dplyr::sample_frac(car.model, 0.6)
 
 usethis::use_data(cve.nist, cpe.nist, cwe.views, cwe.categories, cwe.weaknesses,
                   capec.views, capec.categories, capec.patterns, capec.relations,
