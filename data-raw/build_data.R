441d3901482a03a3d1b288f8f0a5e0f157a15ee5
Humbert Costas <humbert.costas@gmail.com>
2022-04-16 15:22:39 +0200

    Removed comments

--- data-raw/build_data.R
+++ data-raw/build_data.R
@@ -1,6 +1,3 @@
-# for(f in list.files(path = "data", pattern = "*.rda", full.names = T)) load(f)
-# rm(f)
-
 source("data-raw/mitre-cwe.R")
 source("data-raw/mitre-attck.R")
 source("data-raw/mitre-capec.R")
@@ -9,23 +6,6 @@ source("data-raw/mitre-shield.R")
 source("data-raw/mitre-engage.R")
 source("data-raw/nist-cpe.R")
 source("data-raw/nist-cve.R")
-
-# Build sample data sets
-# set.seed(42)
-# cpe.nist <- dplyr::sample_frac(cpe.nist, 0.0002)
-# cve.nist <- dplyr::sample_frac(cve.nist, 0.004)
-# cwe.weaknesses <- cwe.weaknesses[cwe.weaknesses$Status != "Deprecated", ]
-# cwe.weaknesses <- dplyr::sample_frac(cwe.weaknesses, 0.5)
-# cwe.categories <- cwe.weaknesses[cwe.categories$Status != "Deprecated", ]
-# cwe.categories <- dplyr::sample_frac(cwe.categories, 0.5)
-# capec.patterns <- dplyr::sample_frac(capec.patterns, 0.5)
-# capec.relations <- dplyr::sample_frac(capec.relations, 0.4)
-# attck.relations <- dplyr::sample_frac(attck.relations, 0.4)
-# attck.techniques <- dplyr::sample_frac(attck.techniques, 0.5)
-# attck.software <- dplyr::sample_frac(attck.software, 0.5)
-# car.analytics <- dplyr::sample_frac(car.analytics, 0.5)
-# car.model <- dplyr::sample_frac(car.model, 0.5)
-# shield.relations <- dplyr::sample_frac(shield.relations, 0.5)
 
 standards <- list(cpe = list(cpe.nist = cpe.nist),
                   cve = list(cve.nist = cve.nist),
@@ -62,16 +42,5 @@ standards <- list(cpe = list(cpe.nist = cpe.nist),
                              car.coverage = car.coverage,
                              car.relations = car.relations)
                   )
-
-# usethis::use_data(cve.nist, cpe.nist, cwe.views, cwe.categories, cwe.weaknesses,
-#                   capec.views, capec.categories, capec.patterns, capec.relations,
-#                   car.analytics, car.model, car.sensors, car.implementations,
-#                   car.coverage, car.relations, shield.tactics, shield.techniques,
-#                   shield.procedures, shield.use_cases, shield.opportunities,
-#                   shield.relations, attck.tactics, attck.techniques, attck.mitigations,
-#                   attck.groups, attck.software, attck.data_component,
-#                   attck.data_relations, attck.relations, engage.activities,
-#                   engage.approaches, engage.goals, engage.relations,
-#                   compress = "xz", overwrite = TRUE, internal = TRUE)
 
 usethis::use_data(standards, compress = "xz", overwrite = TRUE, internal = TRUE)
