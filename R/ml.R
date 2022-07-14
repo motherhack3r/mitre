258e2713541d148214491ba21bf2b09b7cd15342
Humbert Costas <humbert.costas@gmail.com>
2022-07-14 18:43:11 +0200

    Added createTrainSample used to create training sets for sanjayasubedi NER experiment

--- R/ml.R
+++ R/ml.R
@@ -1,3 +1,137 @@
+# REF: https://sanjayasubedi.com.np/deeplearning/training-ner-with-huggingface-transformer/
+
+#' Title
+#'
+#' @param cpes
+#' @param num_samples
+#' @param samples_mix
+#'
+#' @return
+#' @export
+createTrainSample <- function(cpes = data.frame(), num_samples = 5000,
+                              samples_mix = c(min = 0.6,
+                                              med = 0.25,
+                                              max = 0.15)) {
+  # part is not included in title
+  df_ner <- cpes %>% select(id, title, vendor, product, version)
+  # remove rows with escaped chars due to regex for tagging
+  df_ner <- df_ner[!grepl(pattern = "\\\\", df_ner$vendor), ]
+  df_ner <- df_ner[!grepl(pattern = "\\\\", df_ner$product), ]
+  df_ner <- df_ner[!grepl(pattern = "\\\\", df_ner$version), ]
+  # replace WFN _ to space
+  df_ner$vendor <- stringr::str_replace_all(df_ner$vendor, "_", " ")
+  df_ner$product <- stringr::str_replace_all(df_ner$product, "_", " ")
+  # remove titles with equal vendor and product
+  df_ner <- df_ner[which(df_ner$vendor != df_ner$product), ]
+  # lowercase title
+  df_ner$title <- tolower(df_ner$title)
+  # vendor entities candidates
+  df_ner$train_v <- rep(F, nrow(df_ner))
+  df_ner$train_v <- stringr::str_detect(df_ner$title, stringr::fixed(df_ner$vendor))
+  # product entities candidates
+  df_ner$train_p <- rep(F, nrow(df_ner))
+  df_ner$train_p <- stringr::str_detect(df_ner$title, stringr::fixed(df_ner$product))
+  # version entities candidates
+  df_ner$train_r <- rep(F, nrow(df_ner))
+  df_ner$train_r <- stringr::str_detect(df_ner$title, stringr::fixed(df_ner$version))
+  # Keep only titles with all entities
+  df_ner <- df_ner %>% filter(train_v & train_p & train_r) %>% select(-train_v, -train_p, -train_r)
+
+  # Add tags
+  df_ner$annotated <- df_ner$title
+
+  ## vendor + product + version
+  df_ner$annotated <- str_replace_all(string = df_ner$annotated,
+                                      pattern = paste0("(.*)(", df_ner$vendor,")(\\s.*)(", df_ner$product,")(.*)(", df_ner$version,")(.*)"),
+                                      replacement = "\\1\\\\[\\2\\]\\(cpe_vendor\\)\\3\\[\\4\\]\\(cpe_product\\)\\5\\[\\6\\]\\(cpe_version\\)\\7\\")
+
+  ## Keep rows that miss a cpe element
+  df_not_ner <- df_ner[which(!grepl(pattern = ".*\\]\\(cpe_vendor\\).*\\]\\(cpe_product\\).*\\]\\(cpe_version\\).*", df_ner$annotated)), ]
+  df_ner_vpr <- df_ner[which(grepl(pattern = ".*\\]\\(cpe_vendor\\).*\\]\\(cpe_product\\).*\\]\\(cpe_version\\).*", df_ner$annotated)), ]
+
+  ## product + version
+  df_not_ner$annotated <- str_replace_all(string = df_not_ner$annotated,
+                                      pattern = paste0("(.*)(", df_not_ner$product,")(.*)(", df_not_ner$version,")(.*)"),
+                                      replacement = "\\1\\\\[\\2\\]\\(cpe_product\\)\\3\\[\\4\\]\\(cpe_version\\)\\5")
+  df_ner_pr <- df_not_ner[which(grepl(pattern = ".*\\]\\(cpe_product\\).*\\]\\(cpe_version\\).*", df_not_ner$annotated)), ]
+  df_not_ner <- df_not_ner[which(!grepl(pattern = ".*\\]\\(cpe_product\\).*\\]\\(cpe_version\\).*", df_not_ner$annotated)), ]
+
+  ## vendor + product
+  df_not_ner$annotated <- str_replace_all(string = df_not_ner$annotated,
+                                          pattern = paste0("(.*)(", df_not_ner$vendor,")(.*)(", df_not_ner$product,")(.*)"),
+                                          replacement = "\\1\\\\[\\2\\]\\(cpe_vendor\\)\\3\\[\\4\\]\\(cpe_product\\)\\5")
+  df_ner_vp <- df_not_ner[which(grepl(pattern = ".*\\]\\(cpe_vendor\\).*\\]\\(cpe_product\\).*", df_not_ner$annotated)), ]
+  df_not_ner <- df_not_ner[which(!grepl(pattern = ".*\\]\\(cpe_vendor\\).*\\]\\(cpe_product\\).*", df_not_ner$annotated)), ]
+
+  ## product
+  df_not_ner$annotated <- str_replace_all(string = df_not_ner$annotated,
+                                          pattern = paste0("(.*)(", df_not_ner$product,")(.*)"),
+                                          replacement = "\\1\\\\[\\2\\]\\(cpe_product\\)\\3")
+  df_ner_p <- df_not_ner[which(grepl(pattern = ".*\\]\\(cpe_product\\).*", df_not_ner$annotated)), ]
+  df_not_ner <- df_not_ner[which(!grepl(pattern = ".*\\]\\(cpe_product\\).*", df_not_ner$annotated)), ]
+
+  df_ner <- rbind(df_ner_vpr, df_ner_vp, df_ner_pr, df_ner_p)
+  rm(df_ner_vpr, df_ner_vp, df_ner_pr, df_ner_p)
+
+  # Select rows for training set
+  #  - min_vendors with less than 0.9 titles
+  #  - med_vendors from 0.9 to 0.9975 titles
+  #  - max_vendors more than 0.9975 titles (outliers)
+  min_vendors <- table(cpes$vendor)
+  sts_min <- as.integer(quantile(min_vendors, 0.9))
+  sts_max <- as.integer(quantile(min_vendors, 0.9975))
+
+  max_vendors <- names(min_vendors[min_vendors > sts_max])
+  min_vendors <- names(min_vendors[min_vendors < sts_min])
+  med_vendors <- unique(c(min_vendors, max_vendors))
+  med_vendors <- sort(unique(cpes$vendor[!(cpes$vendor %in% med_vendors)]))
+
+  train_min <- df_ner[which(df_ner$vendor %in% min_vendors),]
+  train_med <- df_ner[which(df_ner$vendor %in% med_vendors),]
+  train_max <- df_ner[which(df_ner$vendor %in% max_vendors),]
+
+  nbias <- num_samples %% 3
+
+  df_min <- mitre::cpe_train_set(train_min, round(floor(num_samples) * samples_mix["min"]))
+  df_med <- mitre::cpe_train_set(train_med, round((floor(num_samples) + nbias) * samples_mix["med"]))
+  df_max <- mitre::cpe_train_set(train_max, round(floor(num_samples) * samples_mix["max"]))
+
+
+  # df_min <- mitre::cpe_train_set(train_min, floor(num_samples/3))
+  # df_med <- mitre::cpe_train_set(train_med, floor(num_samples/3) + nbias)
+  # df_max <- mitre::cpe_train_set(train_max, floor(num_samples/3))
+
+  train_df <- bind_rows(df_min, df_med, df_max)
+
+  train_df <- train_df %>% select(id) %>%
+    left_join(df_ner, by = "id") %>%
+    sample_n(nrow(train_df)) %>% select(title, annotated)
+
+
+  # Select rows for training set
+  #  - min_vendors with less than 100 titles
+  #  - med_vendors with 100 to 1000 titles
+  #  - max_vendors with more than 1000 titles
+
+  # min_vendors <- table(df_ner$vendor)
+  # max_vendors <- names(min_vendors[min_vendors > 1000])
+  # min_vendors <- names(min_vendors[min_vendors < 100])
+  # med_vendors <- unique(c(min_vendors, max_vendors))
+  # med_vendors <- sort(unique(df_ner$vendor[!(df_ner$vendor %in% med_vendors)]))
+
+  # train_min <- df_ner[which(df_ner$vendor %in% min_vendors),] %>%
+  #   sample_n(round(num_samples * samples_mix["min"]))
+  # train_med <- df_ner[which(df_ner$vendor %in% med_vendors),] %>%
+  #   sample_n(round(num_samples * samples_mix["med"]))
+  # train_max <- df_ner[which(df_ner$vendor %in% max_vendors),] %>%
+  #   sample_n(round(num_samples * samples_mix["max"]))
+
+  # train_df <- bind_rows(train_min, train_med, train_max)
+
+  # train_df <- train_df %>% sample_n(nrow(train_df)) %>% select(title, annotated)
+
+  return(train_df)
+}
 
 #' Title
 #'
@@ -7,7 +141,6 @@
 #' @return
 #' @export
 cpe_train_set <- function(cpes = mitre::cpe.nist, num_samples = 1000) {
-  suppressPackageStartupMessages(library(dplyr))
   df <- cpes %>%
     select(id, title, vendor, product, version) %>%
     mutate(title_len = nchar(title)) %>%
@@ -44,7 +177,7 @@ cpe_train_set <- function(cpes = mitre::cpe.nist, num_samples = 1000) {
 #'  df <- ner_tags_cpe()
 #' }
 ner_tags_cpe <- function(cpes = mitre::cpe.nist, overlap = TRUE, columns = "all") {
-  df <- cpes[ , c("title", "vendor", "product", "version")]
+  df <- cpes[ , c("id", "title", "vendor", "product", "version")]
 
   df$title <- tolower(df$title)
   df$vendor <- stringr::str_replace_all(string = df$vendor, pattern = "\\\\",  replacement = "")
@@ -215,31 +348,6 @@ ner_offsets_cpe <- function(cpes = mitre::cpe.nist, overlap = TRUE, columns = "a
   return(df)
 }
 
-#' Title
-#'
-#' @param taste
-#' @param add_tab
-#' @param add_enter
-#'
-#' @return
-#' @export
-ml_cpe_valid_chars <- function(taste = c("char", "dec", "hex")[1],
-                               add_tab = FALSE, add_enter = FALSE) {
-
-  valid_dec_chars <- c(32,33,38,40,41, 43:58, 65:90, 97:122)
-  if (add_enter) valid_dec_chars <- c(10, valid_dec_chars)
-  if (add_tab) valid_dec_chars <- c(9, valid_dec_chars)
-
-  if (taste == "char") {
-    valid_chars <- sapply(valid_dec_chars, DescTools::AscToChar)
-  } else if (taste == "hex") {
-    valid_chars <- sapply(valid_dec_chars, DescTools::DecToHex)
-  } else {
-    return(valid_dec_chars)
-  }
-
-  return(valid_chars)
-}
 
 #' Title
 #'
