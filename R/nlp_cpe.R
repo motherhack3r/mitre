5b0e07588f1fcf5ce0ef5b72b9af6c3d8aafbf56
humbert <humbert.costas@es.nestle.com>
2022-08-30 10:55:45 +0200

    Add function for prepare SCCM dataset

--- R/nlp_cpe.R
+++ R/nlp_cpe.R
@@ -284,6 +284,72 @@ cpe_local_inventory <- function(df = getInventory(), verbose = FALSE) {
   return(df)
 }
 
+
+#' Title
+#'
+#' @param path_sccm path to sccm component definitions csv file
+#' @param verbose logical
+#'
+#' @return data.frame
+#' @export
+cpe_sccm_inventory <- function(path_sccm = "inst/extdata/sccm_component_definitions.csv",
+                               verbose = FALSE) {
+  df <- read.csv(path_sccm, header = F, col.names = c("product", "vendor", "version"))
+  # Add uuid and copy to temporal df
+  # df$uuid <- uuid::UUIDgenerate(n = nrow(df))
+  df$id <- 1:nrow(df)
+  df_sccm <- df
+
+  # Clean vendor strings
+  df_sccm$wfn_vendor <- iconv(df_sccm$vendor, from = "UTF-8", to = 'ASCII//TRANSLIT')
+  df_sccm$wfn_vendor <- stringr::str_replace_all(df_sccm$wfn_vendor, "\\?", "")
+  df_sccm$wfn_vendor <- stringr::str_trim(df_sccm$wfn_vendor)
+  df_sccm$bad_vendor <- (stringr::str_count(df_sccm$wfn_vendor, "[^a-zA-Z0-9 \\.]")
+                         / sapply(df_sccm$wfn_vendor, nchar)
+                         ) > 0.2
+  df_vendor <- df_sccm[!df_sccm$bad_vendor, c("id", "wfn_vendor")]
+  df_vendor <- dplyr::rename(df_vendor, vendor = wfn_vendor)
+  df_vendor$wfn_vendor <- mitre::cpe_wfn_vendor(df_vendor$vendor)
+  df_vendor <- df_vendor[stringr::str_length(df_vendor$wfn_vendor) > 1, c("id", "wfn_vendor")]
+  df_vendor <- dplyr::rename(df_vendor, vendor = wfn_vendor)
+
+  # Clean product strings
+  df_sccm$wfn_product <- iconv(df_sccm$product, from = "UTF-8", to = 'ASCII//TRANSLIT')
+  df_sccm$wfn_product <- stringr::str_replace_all(df_sccm$wfn_product, "\\?", "")
+  df_sccm$wfn_product <- stringr::str_trim(df_sccm$wfn_product)
+  df_sccm$bad_product <- (stringr::str_count(df_sccm$wfn_product, "[^a-zA-Z0-9 \\.]")
+                         / sapply(df_sccm$wfn_product, nchar)
+  ) > 0.2
+  df_product <- df_sccm[!df_sccm$bad_product, c("id", "wfn_product")]
+  df_product <- dplyr::rename(df_product, product = wfn_product)
+  df_product$wfn_product <- mitre::cpe_wfn_product(df_product$product)
+  df_product <- df_product[stringr::str_length(df_product$wfn_product) > 1, c("id", "wfn_product")]
+  df_product <- dplyr::rename(df_product, product = wfn_product)
+
+  # Clean version strings
+  df_sccm$wfn_version <- stringr::str_replace_all(df_sccm$version, "(.*)(\\s|,|-)+v*([\\d|\\.]+)(.*)$", "\\3")
+  df_sccm$wfn_version[(grepl("\\d+(, \\d+)+", df_sccm$wfn_version))] <- gsub(", ", ".", df_sccm$wfn_version[(grepl("\\d+(, \\d+)+", df_sccm$wfn_version))])
+  df_version <- df_sccm[, c("id", "wfn_version")]
+  df_version <- dplyr::rename(df_version, version = wfn_version)
+
+  df_inv <- dplyr::inner_join(df_vendor, df_product, by = "id")
+  df_inv <- dplyr::inner_join(df_inv, df_version, by = "id")
+
+  # df_inv <- dplyr::full_join(df_version, df_vendor, by = "id")
+  df_inv$vendor[is.na(df_inv$vendor)] <- ""
+  # df_inv <- dplyr::full_join(df_inv, df_product, by = "id")
+  df_inv$product[is.na(df_inv$product)] <- ""
+
+  df_inv$title = paste(df_inv$vendor, df_inv$product, df_inv$version)
+  df_inv$title <- stringr::str_replace_all(df_inv$title, "\\s+", " ")
+  df_inv$title <- stringr::str_trim(df_inv$title)
+
+  df <- dplyr::left_join(df, df_inv[, c("id", "title")], by = "id")
+  df <- df[, c("title", "vendor", "product", "version")]
+
+  return(df)
+}
+
 #' Title
 #'
 #' @param x character
@@ -292,13 +358,42 @@ cpe_local_inventory <- function(df = getInventory(), verbose = FALSE) {
 #' @export
 cpe_wfn_vendor <- function(x = "Microsoft Corporation") {
   # Normalize vendor: First apply translit, then remove bad words and HTML entities
-  x <- iconv(x, to = 'ASCII//TRANSLIT')
-  x <- stringr::str_replace_all(x, "(?i)(\\s|,)+(corporation|ltd|llc|inc).{0,1}$", "")
+  x <- iconv(x, to = 'ASCII//TRANSLIT', sub = "")
+  x <- stringr::str_replace_all(x, "(?i)\\([c|tm|r]\\)", "")
+  x <- stringr::str_replace_all(x, "(?i)^\\(([^\\)]+)\\){0,1}", "\\1")
+  x <- stringr::str_replace_all(x, "(?i)(\\s|,)+(co|corp|corporation|ltd|llc|inc|incorporated)\\.{0,1}$", "")
   x <- stringr::str_replace_all(x, "(?i)(\\s|,)+software(\\s|,){0,1}", " ")
   x <- stringr::str_trim(x)
   x <- stringr::str_replace_all(x, "(?i)(\\s|,)+foundation(\\s|,){0,1}", " ")
   x <- stringr::str_trim(x)
+  x <- stringr::str_replace_all(x, "(?i)(\\s|,)+systems(\\s|,){0,1}", " ")
+  x <- stringr::str_trim(x)
+  x <- stringr::str_replace_all(x, "(?i)(\\s|,)+technologies(\\s|,){0,1}", " ")
+  x <- stringr::str_trim(x)
+  x <- stringr::str_replace_all(x, "(?i)(\\s|,)+limited(\\s|,){0,1}", " ")
+  x <- stringr::str_trim(x)
+  x <- stringr::str_replace_all(x, "(?i)(\\s|,)+systems(\\s|,){0,1}", " ")
+  x <- stringr::str_trim(x)
+  x <- stringr::str_replace_all(x, "(?i)(\\s|,|-)*(development|core){0,1}(\\s|,|-)+(team){0,1}$", " ")
+  x <- stringr::str_trim(x)
   x <- sapply(x, function(y) xml2::xml_text(xml2::read_html(paste0("<x>",y,"</x>"))))
+
+
+  x <- stringr::str_replace_all(x, "^[^a-zA-Z0-9]+$", "")
+  x <- stringr::str_replace_all(x, "^[^a-zA-Z0-9]+\\s([a-zA-Z0-9].+)$", "\\1")
+  x <- stringr::str_replace_all(x, "^\\${0,1}\\{(.+)\\}$", "")
+  x <- stringr::str_replace_all(x, "^\\$(.+)\\$$", "")
+  x <- stringr::str_replace_all(x, "\\(\\)", "")
+  x <- stringr::str_replace_all(x, "\\[\\]", "")
+  x <- stringr::str_replace_all(x, "^'([^']+)'$", "\\1")
+  x <- stringr::str_replace_all(x, "^\"([^']+)\"$", "\\1")
+
+  # CUSTOM MODIFICATORS
+  # sap_xx --> sap
+  x <- stringr::str_replace_all(x, "sap_[a-z]{2}", "sap")
+
+  x[x == "NA"] <- NA
+  x[is.na(x)] <- ""
 
   return(x)
 }
@@ -318,7 +413,7 @@ cpe_wfn_product <- function(x = "Oracle VM VirtualBox 6.1.34") {
   x <- stringr::str_replace_all(x, "\\s", "_")
   x <- stringr::str_replace_all(x, "_\\-_.*$", "")
   x <- stringr::str_replace_all(x, "_\\d+\\.\\d+.*$", "")
-  x <- stringr::str_replace_all(x, "(?i)_(x|amd)(32|64|86).*$", "")
+  x <- stringr::str_replace_all(x, "(?i)_{0,1}(x|amd)(32|64|86).*$", "")
   x <- stringr::str_replace_all(x, "(?i)_for_.*$", "")
   x <- stringr::str_replace_all(x, "\\(\\)", "")
   x <- stringr::str_replace_all(x, "(?i)\\(r\\)", "")
